e71213be4a18027fd6c9c6ef5a1806eb767c19d0
jvlguanco <jvlguanco@gmail.com>
2023-11-21 20:25:48 +0800

    Added Helper Functions

---
+++ lib/src/utils/constants/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TColors {
+  TColors._();
+  
+  static const Color primaryColor = Color(0xFF000000);
+  static const Color secondaryColor1 = Color(0xFFF26430);
+  static const Color secondaryColor2 = Color(0xFF009B72);
+  static const Color secondaryColor3 = Color(0xFF009DDC);
+
+  static const Color succerssColor = Color(0xFF84E1BC);
+  static const Color warningColor = Color(0xFFFACA15);
+  static const Color errorColor = Color(0xFFF8B4B4);
+  static const Color infoColor = Color(0xFFA4CAFE);
+  static const Color darkColor = Color(0xFFD1D5DB);
+
+  static const Color primaryTextColor = Color(0xFF000000);
+  static const Color secondaryTextColor = Color(0xFF444466);
+  static const Color accentTextColor = Color(0xFF0F027A);
+}
---
+++ lib/src/utils/constants/sizes.dart
@@ -0,0 +1,41 @@
+class TSizes {
+  // Padding and Margins
+  static const double xs = 4.0;
+  static const double s = 8.0;
+  static const double m = 16.0;
+  static const double l = 24.0;
+  static const double xl = 32.0;
+
+  // Font Sizes
+  static const double display_small =  36.0;
+  static const double display_medium = 48.0;
+  static const double display_large =  60.0;
+
+  static const double heading_small =  24.0;
+  static const double heading_medium = 28.0;
+  static const double heading_large =  32.0;
+
+  static const double title_small = 14.0;
+  static const double title_medium = 16.0;
+  static const double title_large = 22.0;
+
+  static const double label_small = 11.0;
+  static const double label_medium = 12.0;
+  static const double label_large = 14.0; 
+
+  static const double body_small = 12.0;
+  static const double body_medium = 14.0;
+  static const double body_large = 16.0;
+
+  // Icon Sizes
+  static const double icon_xs = 16.0;
+  static const double icon_s = 20.0;
+  static const double icon_m = 24.0;
+  static const double icon_l = 28.0;
+  static const double icon_xl = 32.0;
+
+  // Button Sizes
+  static const double button_height = 18.0; 
+  static const double button_width = 120.0;
+  static const double button_radius = 12.0;
+}
